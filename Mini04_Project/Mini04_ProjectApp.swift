31100dfb2d1f8fb8a342478273d75d33ecd9518c
GustavoHoreste <101297032+GustavoHoreste@users.noreply.github.com>
2024-03-04 17:46:45 -0300

    first

---
+++ Mini04_Project/Mini04_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mini04_ProjectApp.swift
+//  Mini04_Project
+//
+//  Created by Gustavo Horestee Santos Barros on 04/03/24.
+//
+
+import SwiftUI
+
+@main
+struct Mini04_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
