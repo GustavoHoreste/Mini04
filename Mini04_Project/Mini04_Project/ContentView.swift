a80122cc9e53bf9406b079649d107dfeee8f18c1
GustavoHoreste <101297032+GustavoHoreste@users.noreply.github.com>
2024-03-04 17:56:10 -0300

    Add files via upload

---
+++ Mini04_Project/Mini04_Project/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Mini04_Project
+//
+//  Created by Gustavo Horestee Santos Barros on 04/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+            //Teste Familia
+            Text("Como vai")?
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
