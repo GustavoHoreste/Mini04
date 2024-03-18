9308c4a386e965d8d01c62747de02f8a7215dfbf
lippysz <afchinen@gmail.com>
2024-03-18 15:15:19 -0300

    garantia

---
+++ Mini04_Project/Mini04_Project/UIKit/Gameplay/Components/ObjectName.swift
@@ -0,0 +1,20 @@
+//
+//  ObjectName.swift
+//  Mini04_Project
+//
+//  Created by André Felipe Chinen on 18/03/24.
+//
+
+import UIKit
+
+class ObjectName: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Mini04_Project/Mini04_Project/UIKit/Gameplay/ItemsToFindModel.swift
@@ -0,0 +1,40 @@
+//
+//  ItemsToFindModel.swift
+//  Mini04_Project
+//
+//  Created by André Felipe Chinen on 18/03/24.
+//
+
+import Foundation
+
+class ItemsToFindModel {
+    var objects: [String] = ["Armario", "Bicicleta", "Bone", "Cadeira", "Calçados", "Cama", "Camisa", "Caneca", "Caneta", "Chave de fenda", "Chuveiro", "Clipe de papel", "Cola Bastao", "Colher", "Espatula", "Flauta", "Frigideira", "Geladeira", "Impressora", "Livro:Caderno", "Lixeira", "Martelo", "Mesa", "Mochila", "Oculos", "Papel higienico", "Parafusadeira", "Pente", "Pilha", "Porta", "Regua", "Shorts:Calca", "Sofa", "TV", "Teclado", "Tesoura", "Toalha", "Vaso sanitario", "Ventilador"]
+    var colors: [String] = ["blue", "green", "pink", "purple", "red", "white", "yellow"]
+    var timeRemaining = 120
+    var toFindObject: String = ""
+    var numberOfObjects: Int = 0
+    var showPopOver: Bool = false
+    var defeatShow: Bool  = false
+    var objectIsRigt: Bool = false
+    var findedObjects: [String] = []{
+        didSet{
+            numberOfObjects = findedObjects.count
+        }
+    }
+    
+    func chooseObject() {
+        if numberOfObjects < 10 {
+            let actualObject = toFindObject
+            while(actualObject == toFindObject){
+                toFindObject = objects.filter{!findedObjects.contains($0)}.randomElement()!
+            }
+        } else {
+            toFindObject = "Nenhum"
+        }
+    }
+    
+    func findedObject() {
+        findedObjects.append(toFindObject)
+        chooseObject()
+    }
+}
