3e1f4c1b06937c1ea8cf1990e77a54c322038286
GustavoHoreste <101297032+GustavoHoreste@users.noreply.github.com>
2024-03-18 19:46:57 -0300

    Classes de gerenciamento shareplay adicionado

--- Mini04_Project/Mini04_Project/Services/SharePlay/SharePlayModel.swift
+++
@@ -1,8 +0,0 @@
-//
-//  SharePlayModel.swift
-//  Mini04_Project
-//
-//  Created by Luca Lacerda on 15/03/24.
-//
-
-import Foundation
---
+++ Mini04_Project/Mini04_Project/Services/SharePlay/SharePlayWorker.swift
@@ -0,0 +1,59 @@
+//
+//  SharePlayWorker.swift
+//  Mini04_Project
+//
+//  Created by Gustavo Horestee Santos Barros on 18/03/24.
+//
+
+import SwiftUI
+import Combine
+import GroupActivities
+
+final class SharePlayWorker{
+    ///lista de players
+    ///valores recebidos
+    
+    ///funcao que Start SharePlay
+    public func startSession(){
+        Task{
+            do{
+                _ = try await WhereWhereActivity().activate()
+            }catch{
+                print("Error in init session [SharePlayWorker.startSession] - ", error.localizedDescription)
+            }
+        }
+    }
+    
+    
+    ///func que envia os dodos do Player local
+    public func sendPlayerData(){
+        
+    }
+    
+    
+    ///func que envia os pontos
+    public func sendPoints(){
+        //envia os dados para todo mundo
+    }
+    
+    
+    ///func que envia o powerUp para o inimingo
+    public func sendPowerUps(){
+        
+    }
+    
+    
+    ///func que confgura shareplay e recebe o dado do shareplay
+    
+    
+    ///func que verifica estado da sesssion
+    private func verifyStatusSession(){
+        
+    }
+    
+//    ///
+//    private func newParticipant(){
+//
+//    }
+}
+
