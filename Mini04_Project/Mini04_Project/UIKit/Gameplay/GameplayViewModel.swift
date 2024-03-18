4a032b58a404c8cceeef4a54fd6ac58d8f55d47a
Gabriel Cardoso <74602964+gabriel144p@users.noreply.github.com>
2024-03-18 15:04:06 -0300

    ViewModel

---
+++ Mini04_Project/Mini04_Project/UIKit/Gameplay/GameplayViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  GameplayViewModel.swift
+//  Mini04_Project
+//
+//  Created by GABRIEL Ferreira Cardoso on 18/03/24.
+//
+
+import Foundation
+
+class GameplayViewModel: ObservableObject {
+    
+    @Published var timeRemaing = 120
+}
