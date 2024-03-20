d14f8e81c10ab5f2ced925ec52d196e72b756038
GustavoHoreste <101297032+GustavoHoreste@users.noreply.github.com>
2024-03-20 10:50:55 -0300

    funcionando main

--- Mini04_Project/Mini04_Project/SwiftUI/Lobby/LobbyView.swift
+++ Mini04_Project/Mini04_Project/SwiftUI/Lobby/LobbyView.swift
@@ -24,79 +24,75 @@ struct LobbyView: View {
     
     var body: some View {
         VStack {
-            
-            Button("Adicione seu amigo") {
-                verifyStausSession()
-            //Menu
-            HStack {
-                Button{navigationCoordinator.pop()}label: {
-                    Image(systemName: "chevron.left.circle.fill")
-                        .resizable()
-                        .frame(width: 70, height: 70)
-                }
-                
-                Spacer()
-                
-                Button(action: {navigationCoordinator.present(sheet: .config)}, label: {
-                    Image(systemName: "gearshape.circle.fill")
-                        .resizable()
-                        .frame(width: 70, height: 70)
+                //Menu
+                HStack {
+                    Button{navigationCoordinator.pop()}label: {
+                        Image(systemName: "chevron.left.circle.fill")
+                            .resizable()
+                            .frame(width: 70, height: 70)
+                    }
                     
-                })
+                    Spacer()
+                    
+                    Button(action: {navigationCoordinator.present(sheet: .config)}, label: {
+                        Image(systemName: "gearshape.circle.fill")
+                            .resizable()
+                            .frame(width: 70, height: 70)
+                        
+                    })
+                    
+                }
+                .foregroundStyle(.gray)
+                .padding()
                 
-            }
-            .foregroundStyle(.gray)
-            .padding()
-            
-            //Title
-            Text("Sala de espera")
-                .font(.title)
-                .bold()
-            
-            //Grid
-            ScrollView {
-                LazyVGrid(columns: adaptiveColumns, alignment: .center, spacing: 20) {
-                    ForEach(data, id: \.self) { number in
-                        ZStack {
-                            RoundedRectangle(cornerRadius: 52.5)
-                                .frame(width: 156, height: 65)
-                                .foregroundStyle(colors[number%7])
+                //Title
+                Text("Sala de espera")
+                    .font(.title)
+                    .bold()
+                
+                //Grid
+                ScrollView {
+                    LazyVGrid(columns: adaptiveColumns, alignment: .center, spacing: 20) {
+                        ForEach(data, id: \.self) { number in
+                            ZStack {
+                                RoundedRectangle(cornerRadius: 52.5)
+                                    .frame(width: 156, height: 65)
+                                    .foregroundStyle(colors[number%7])
+                            }
                         }
                     }
                 }
+                .padding()
+                
+                Spacer()
+                
+                //Buttons
+                Button(action: {verifyStausSession()}, label: {
+                    Text("Adicione seu amigo")
+                })
+                
+                Button(action: {navigationCoordinator.push(.gameplay)}, label: {
+                    Text("Começar")
+                        .padding()
+                        .foregroundStyle(.white)
+                        .background(.gray)
+                        .font(.title)
+                        .clipShape(.capsule)
+                    
+                })
+                
+                
+                
+            }.task {
+                for await session in WhereWhereActivity.sessions(){
+                    sharePlayVM.configurationSessin(session)
+                }
             }
-            .padding()
-            
-            Spacer()
-            
-            //Buttons
-            Button(action: {verifyStausSession()}, label: {
-                Text("Adicione seu amigo")
-            })
-            
-            Button(action: {navigationCoordinator.push(.gameplay)}, label: {
-                Text("Começar")
-                    .padding()
-                    .foregroundStyle(.white)
-                    .background(.gray)
-                    .font(.title)
-                    .clipShape(.capsule)
-    
-            })
-            
-
-            
-        }.task {
-            for await session in WhereWhereActivity.sessions(){
-                sharePlayVM.configurationSessin(session)
-            }
+            .navigationBarBackButtonHidden()
         }
-        .navigationBarBackButtonHidden()
-    }
     
-    
-   ///funcao que verifica o estado da session - se for desativada inicia a session
-   private func verifyStausSession(){
+    ///funcao que verifica o estado da session - se for desativada inicia a session
+    private func verifyStausSession(){
         if groupStateObserver.isEligibleForGroupSession{
             sharePlayVM.startSession()
             return
