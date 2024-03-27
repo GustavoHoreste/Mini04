b50555f4c53e4c8edb8b525b76740372d1f1224d
Luca Lacerda <lucagabriel520@gmail.com>
2024-03-27 14:35:56 -0300

    adjusts

--- Mini04_Project/Mini04_Project/SwiftUI/Lobby/Components/PlayerListCell.swift
+++ Mini04_Project/Mini04_Project/SwiftUI/Lobby/Components/PlayerListCell.swift
@@ -29,19 +29,3 @@ struct PlayerListCell: View {
         }
     }
 }
-
-
-struct UILableRepresentable: UIViewRepresentable {
-    var text: String
-    
-    func makeUIView(context: Context) -> UILabel {
-        let textView = UILabel()
-        textView.font = UIFont.preferredFont(forTextStyle: UIFont.TextStyle.body)
-        textView.textAlignment = .justified
-        return textView
-    }
-    
-    func updateUIView(_ uiView: UILabel, context: Context) {
-        uiView.text = text
-    }
-}
