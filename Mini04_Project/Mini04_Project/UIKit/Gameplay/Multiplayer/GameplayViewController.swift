de8c18c62223a0d671a22f71ab0a53da3020ba82
lippysz <afchinen@gmail.com>
2024-04-05 14:26:44 -0300

    testbutton comentado

--- Mini04_Project/Mini04_Project/UIKit/Gameplay/Multiplayer/GameplayViewController.swift
+++ Mini04_Project/Mini04_Project/UIKit/Gameplay/Multiplayer/GameplayViewController.swift
@@ -15,17 +15,17 @@ class GameplayViewController: UIViewController {
     var multiVM: MultiplayerManagerViewModel
     var navigationCoordinator: Coordinator
     
-    lazy var testButton: UIButton = {
-        let button = UIButton()
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.configuration = .filled()
-        button.configuration?.baseBackgroundColor = .systemPink
-        button.configuration?.title = "Add Power"
-        button.addTarget(self, action: #selector(testAction), for: .touchUpInside)
-        button.widthAnchor.constraint(equalToConstant: 100).isActive = true
-        button.heightAnchor.constraint(equalToConstant: 100).isActive = true
-        return button
-    }()
+//    lazy var testButton: UIButton = {
+//        let button = UIButton()
+//        button.translatesAutoresizingMaskIntoConstraints = false
+//        button.configuration = .filled()
+//        button.configuration?.baseBackgroundColor = .systemPink
+//        button.configuration?.title = "Add Power"
+//        button.addTarget(self, action: #selector(testAction), for: .touchUpInside)
+//        button.widthAnchor.constraint(equalToConstant: 100).isActive = true
+//        button.heightAnchor.constraint(equalToConstant: 100).isActive = true
+//        return button
+//    }()
     
     @objc func testAction() {
         gameplayVM.powers.addPowers()
@@ -44,9 +44,9 @@ class GameplayViewController: UIViewController {
         setupView()
         gameplayVM.timerStart.sizeIncrease()
         
-        view.addSubview(testButton)
-        testButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        testButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+//        view.addSubview(testButton)
+//        testButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+//        testButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
     }
     
     init(multiVM: MultiplayerManagerViewModel, navigationCoordinator: Coordinator) {
