bc243747cd8e156660f4c0e70d8e8496c97afca8
Luca Lacerda <lucagabriel520@gmail.com>
2024-04-10 22:31:38 -0300

    onboard files

---
+++ Mini04_Project/Mini04_Project/SwiftUI/Onboard/OnboardView.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardView.swift
+//  Mini04_Project
+//
+//  Created by Luca Lacerda on 10/04/24.
+//
+
+import SwiftUI
+
+struct OnboardView: View {
+    var body: some View {
+        Text("teste")
+    }
+}
+
+#Preview {
+    OnboardView()
+}
