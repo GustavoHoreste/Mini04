c6e696882ea7abab84cd3212ae9392bcf95bdba1
Gabriel Cardoso <74602964+gabriel144p@users.noreply.github.com>
2024-04-16 20:16:23 -0300

    Update

--- Mini04_Project/Mini04_Project/SwiftUI/Profile/CharPicker.swift
+++ Mini04_Project/Mini04_Project/SwiftUI/Profile/CharPicker.swift
@@ -9,30 +9,29 @@ import SwiftUI
 
 struct CharPicker: View {
     @EnvironmentObject private var vm: ProfileViewViewModel
-    @Binding var selectedImage: String
     let columnsGrid = 2
     
     var body: some View {
-            HStack {
-                ForEach(vm.imageNames, id: \.self) { imageName in
-                    Image(imageName)
-                        .resizable()
-                        .frame(width: 60, height: 60)
-                        .opacity(imageName == vm.newImage ? 0.5 : 1.0)
-                        .scaleEffect(imageName == vm.newImage ? 1.3 : 1.0)
-                        .onTapGesture {
-                            vm.newImage = imageName
-                            vm.imageTapped()
-                        }
-                }
-                
+        LazyHGrid(rows: [GridItem(), GridItem()]) {
+            ForEach(vm.imageNames, id: \.self) { imageName in
+                Image(imageName)
+                    .resizable()
+                    .frame(width: 60, height: 60)
+                    .opacity(imageName == vm.newImage ? 0.5 : 1.0)
+                    .scaleEffect(imageName == vm.newImage ? 1.3 : 1.0)
+                    .onTapGesture {
+                        vm.newImage = imageName
+                        vm.imageTapped()
+                    }
             }
-        }
-                
             .padding()
             .padding(.horizontal)
+        }
+        
     }
+    
 }
+
 
 //#Preview {
 //    CharPicker()
